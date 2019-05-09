99b574a520e590418052180059f9e3308b4435bd
KaLvaL <zrt15fsu@uea.ac.uk>
2019-05-09 10:56:06 +0100

    Ending level and scoring screen.

--- main.cpp
+++ main.cpp
@@ -27,12 +27,14 @@ int levels = 1;
 bool enemyCollide = false;
 float startTime = 0;
 float currentTime = 0;
-
+int numCollect = 0;
 
 struct Time
 {
-	int mm = 0, ss = 0, ms = 0;
+	int mm = 0, ss = 0, ms = 0, totalTime = 0, remMS = 0;
 	string min = "", sec = "", msec = "";
+	string time = "";
+	string displayScore = "Time so far: " + time;
 };
 
 Time t;
@@ -44,7 +46,8 @@ vector<Platform*> platforms;
 vector<MovingPlatform*> movingPlatsRight;
 vector<MovingPlatform*> movingPlatsUp;
 vector<Enemy*> enemies;
-string timeString = t.min + ":" + t.sec + ":" + t.msec;
+string collectableDisplay = to_string(numCollect) + " collectables.";
+
 //OPENGL FUNCTION PROTOTYPES
 void display();				//called in winmain to draw everything to the screen
 void reshape(int width, int height);				//called when the window is resized
@@ -54,9 +57,13 @@ void update();				//called in winmain to update variables#
 void texturise();
 void addTextures();
 void applyCollision();
+void calcTime();
+void drawScoringScreen();
+void drawStartScreen();
 GLuint loadPNG(char* name);
 void displayTimer();
 GLuint bg;
+GLuint startScreen;
 
 Level level;
 Player player(950,550, 49, 49, "Assets/platform_gfx/hero/hero.png"); // keep in mind each field/tile size is 50. x=1;
@@ -139,6 +146,14 @@ void keyDown(unsigned char key, int x, int y)
 void keyOperations() {
 	if (keyStates[27]) {
 		exit(0);
+	}
+	if(levels == 0)
+	{
+		if(keyStates[13] || keyStates[10])
+		{
+			level.updateLevel(levels);
+			level.drawLevel(dt);
+		}
 	}
 	if(keyStates['r'])
 	{
@@ -237,16 +252,20 @@ void applyCollision()
 	}
 	for (Entity* c : collectables)//collectables
 	{
-		if (player.AABB(*c))
+		if (player.AABB(*c)) {
 			level.setTile(c->getX(), c->getY());
+			numCollect++;
+		}
 	}
 	for(Entity* l : leaves) //exits to next level
 	{
 		if(player.Leaf(*l)){
 
 			cout << "Sleeping...." << endl;
+			drawScoringScreen();
 			level.generateLevel(level.updateLevel(levels));
-			//Sleep(2000);			
+			//Sleep(2000);
+			//Clear the level vector, generate the level again and draw it.
 		}
 	}
 }
@@ -273,9 +292,51 @@ void drawBackground()
 	//glColor3f(1, 1, 1);
 	glPopMatrix();
 }
+
+void drawStartScreen()
+{
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+		glBindTexture(GL_TEXTURE_2D, startScreen);
+		glColor4f(0, 0, 0, 1);
+		glBegin(GL_POLYGON);
+			glTexCoord2f(0, 0); glVertex2d(0, 0);	 //bottom left
+			glTexCoord2f(1, 0); glVertex2d(screenWidth , 0); //bottom right
+			glTexCoord2f(1, 1); glVertex2d(screenWidth, screenHeight); //top right
+			glTexCoord2f(0, 1); glVertex2d(0, screenHeight);//top left
+		glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+}
+void drawScoringScreen()
+{
+	glPushMatrix();
+	glColor3f(0, 0, 0);
+	glBegin(GL_POLYGON);
+			 glVertex2d(0, 0);	 //bottom left
+			 glVertex2d(800, 0); //bottom right
+			 glVertex2d(800, 600); //top right
+			 glVertex2d(0, 600);//top left
+	glEnd();
+	glPopMatrix();
+	calcTime();
+	displayTimer(collectableDisplay, screenWidth / 2 - 100, screenHeight / 2 + 50, 0.8, 0, 0.2);
+	displayTimer(t.displayScore,screenWidth / 2 - 200,screenHeight / 2, 0.8,0,0.2);
+}
+void calcTime()
+{
+	t.totalTime = currentTime;
+	t.mm = t.totalTime % 60000;
+	t.remMS = currentTime - t.mm;
+	t.ss = t.remMS % 1000 - numCollect;
+	t.min = to_string(t.mm);
+	t.sec = to_string(t.ss);
+	t.time = t.min + ":" + t.sec;
+}
+
 void display()																	
 {	
-
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
 	drawBackground();
@@ -283,37 +344,52 @@ void display()
 	float newTime = glutGet(GLUT_ELAPSED_TIME);
 	dt = (newTime - oldTime) * 0.75;
 	oldTime = newTime;
-	currentTime = glutGet(GLUT_ELAPSED_TIME);
 	
-	camera();
-
-	level.drawLevel(dt);
-	applyCollision();
-
-	enemy1.drawEnemy(550, dt);
-	enemy2.drawEnemy(420, dt);
-
+	
+	if (levels == 0)
+	{
+		drawStartScreen();
+	}
+	if (levels % 2 != 0 && levels != 0) {
+		currentTime = glutGet(GLUT_ELAPSED_TIME);
+		camera();
+		applyCollision();
+		level.drawLevel(dt);
+		player.drawEntity(dt);
+		enemy1.drawEnemy(550, dt);
+		enemy2.drawEnemy(420, dt);
+		for (MovingPlatform* r_p : movingPlatsRight)
+		{
+			r_p->drawPlatformRight(dt);
+		}
+		displayTimer(to_string(currentTime - startTime), camX + 625, camY + 450, 0, 0, 0);
+		player.gravity(dt);
+	}
+	else
+	{
+		drawScoringScreen();
+		//Sleep(2000);
+		level.generateLevel(level.updateLevel(levels));
+	}
+	
 	//drawing and updating moving platforms
 	//for(MovingPlatform* u_p: movingPlatsUp)
 	//{
 	//	u_p->drawPlatformUp(dt);
 	//}
 	
-	for (MovingPlatform* r_p : movingPlatsRight)
-	{
-		r_p->drawPlatformRight(dt);
-	}
-
-	player.drawEntity(dt);
+	
+	
+	
 
 	//displayTimer(to_string(currentTime), camX + 625, camY + 450,0,0,0.5);
 	//displayTimer(to_string(startTime), camX + 625, camY + 350,0, 0.5, 0);
-	displayTimer(to_string(currentTime - startTime), camX + 625, camY + 450,0, 0, 0);
-	//cout << t.min << ":" << t.sec << ":" << t.msec << endl;
+	
+	
 	glFlush();
 	glutSwapBuffers();
 	
-	player.gravity(dt);	
+	
 }
 
 void reshape(int width, int height)		// Resize the OpenGL window
@@ -334,6 +410,7 @@ void reshape(int width, int height)		// Resize the OpenGL window
 void init()
 {
 	glClearColor(0.0,0.0,0.0,1.0);						//sets the clear colour to yellow
+	startScreen = loadPNG("Assets/StartScreen.png");
 	level.generateLevel(levels);
 	bg = loadPNG("Assets/background.png");
 	tiles = level.getEntityVector();
@@ -373,13 +450,15 @@ int main(int argc, char **argv)
 	keyStates['a'] = false;
 	keyStates['d'] = false;
 	keyStates[27] = false;
+	keyStates[13] = false;
+	keyStates[10] = false;
 	keyStates['r'] = false;
 	
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(screenWidth, screenHeight);
    	glutInitWindowPosition(100,100);
 	
-	glutCreateWindow("My Game");
+	glutCreateWindow("Stagnant Instant");
 	
 	init();
 	
